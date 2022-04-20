b82b9ddff01f0b6916350c29f0591646f2ec40cf
samwaugh <sam@handfish.org>
2022-04-20 14:34:20 +1000

    Update and rename bme280.py to bme680.py

--- bme680.py
+++ bme680.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#           bme280.py
-#  Read data from a digital pressure sensor.
-#
-#  Official datasheet available from :
-#  https://www.bosch-sensortec.com/bst/products/all_products/bme280
-#
-# Author : Matt Hawkins
-# Date   : 21/01/2018
-#
-# https://www.raspberrypi-spy.co.uk/
-#
-#--------------------------------------
+
 import smbus
 import sys
 import time
