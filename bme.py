baa4058319f02bb0ba5ebeb10e40797799f67852
samwaugh <sam@handfish.org>
2022-04-21 22:04:54 +1000

    Update bme.py

--- bme.py
+++ bme.py
@@ -13,5 +13,4 @@ sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 
 if sensor.get_sensor_data():
-    print('{0:.2f} {1:.2f} {2:.3f}'.format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity))
-    print(sensor.data)
+    print('{0:.2f} {1:.2f} {2:.2f}'.format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity))
