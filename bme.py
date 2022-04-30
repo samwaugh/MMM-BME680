e386c059db6022054048975a387071857a646876
samwaugh <sam@handfish.org>
2022-04-30 15:38:39 +1000

    Update bme.py

--- bme.py
+++ bme.py
@@ -26,12 +26,16 @@ iaq_tracker = iaq.IAQTracker()
 
 sensor.get_sensor_data()
 temp = sensor.data.temperature
+print(temp)
 hum = sensor.data.humidity
+print(hum)
 press = sensor.data.pressure
+print(press)
 while not sensor.data.heat_stable:
     sensor.get_sensor_data()
     sleep(1)
     
 r_gas_k = sensor.data.gas_resistance/1000
+print(r_gas_k)
 aq = iaq_tracker.getIAQ(sensor.data)
 print("{0:.2f} {1:.2f} {2:.2f} {3:.1f} {4:.2f}".format(temp, hum, press, r_gas_k, aq))
